8fa6456d14c8f90891f1a8c683291725c44a1dd7
dioh0922 <diohound@gmail.com>
2025-07-10 20:41:33 +0900

    fix prompt

--- api/server.ts
+++ api/server.ts
@@ -24,7 +24,7 @@ async function askGemini(){
   const movielist = await readData();
   const response = await ai.models.generateContent({
     model:"gemini-2.5-flash",
-    contents: JSON.stringify(movielist) + "\n以上のjsonから傾向を分析してください。好みのカテゴリとそうでないカテゴリに注目して分析し、原因を考察してください。回答内容にタイトルを含めるのは一切禁止でマークダウン形式で出力してください。"
+    contents: JSON.stringify(movielist) + "\n以上のjsonから傾向を分析してください。好みのカテゴリとそうでないカテゴリに注目して分析し、原因を考察してください。このとき、複数回記録されているものは一つとして分析してください。回答内容にタイトルを含めるのは一切禁止でマークダウン形式で出力してください。"
   });
   return response.text;
 }
